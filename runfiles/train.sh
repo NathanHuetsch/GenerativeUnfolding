f629c186839d46c1247e43b9ab9bf64efb64f300
javivilladamigo <marino@thpys.uni-heidelberg.de>
2023-11-20 09:29:08 +0100

    params changes

--- runfiles/train.sh
+++ runfiles/train.sh
@@ -1,11 +1,11 @@
 #PBS -q gshort
 #PBS -l nodes=1:ppn=1:gpus=1:gshort
 #PBS -l walltime=40:00:00
-#PBS -d /remote/gpu07/huetsch
+#PBS -d /remote/gpu02/marino
 #PBS -o output.txt
 #PBS -e error.txt
 export CUDA_VISIBLE_DEVICES=$(cat $PBS_GPUFILE | sed s/.*-gpu// )
-source venv/bin/activate
+source GenUnfold/bin/activate
 cd GenerativeUnfolding
 
-src train $1
+src train params/inn_sigmoidtest.yaml $1
